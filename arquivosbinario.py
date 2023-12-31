09d449e5d6abec2240b4957cd6820cfb3b094460
Eduardo Alves <eduardoalves090@gmail.com>
2023-12-31 02:22:35 -0300

    convertendo para binario

---
+++ arquivosbinario.py
@@ -0,0 +1,19 @@
+import base64
+
+
+with open("imagem_1.jpg", "rb") as image2string: 
+    converted_string = base64.b64encode(image2string.read()) 
+print (converted_string.decode('utf-8'))
+
+with open('encode.bin', "wb") as file: 
+    file.write(converted_string)
+
+  
+  
+file = open('encode.bin', 'rb') 
+byte = file.read() 
+file.close() 
+  
+decodeit = open('hello_level.jpeg', 'wb') 
+decodeit.write(base64.b64decode((byte))) 
+decodeit.close() 
